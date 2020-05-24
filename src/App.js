e57da111104db4e95fe47539abaec4b4369bf7c7
Ankit <ankit90202@gmail.com>
2020-05-24 20:16:28 +0530

    added bootstrap and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Todo App</div>;
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-4">Left menu</div>
+        <div className="col-md-4">Todo</div>
+        <div className="col-md-4">Right SideBar</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
